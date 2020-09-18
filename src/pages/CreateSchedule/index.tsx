ad539efad3d4d250b8ef726efadd3912a0b11a28
unknown <serjumano17@gmail.com>
2020-09-17 21:46:44 -0300

    create Schedule

--- src/pages/CreateSchedule/index.tsx
+++ src/pages/CreateSchedule/index.tsx
@@ -1,13 +1,69 @@
 import React from 'react'
 
+import * as Styled from './styles'
+
 import Header from './../../containers/Header/index'
+import ContainerPainel from './../../components/TableContainer/index'
+import ContainerHeader from './../../components/TableHeader/index'
+import Input from './../../components/Input/index'
 
 export default function CreateSchedule() {
+  function renderParticipants() {
+    const array = [1, 2, 3, 4]
+
+    return array.map((participant, index) => {
+      return (
+        <Styled.ContentGridContainer key={index} >
+          <Styled.ParticipantContentContainer >
+            <Styled.ParticipantContent>João</Styled.ParticipantContent>
+          </Styled.ParticipantContentContainer>
+
+          <Styled.ParticipantContentContainer >
+            <Styled.ParticipantContent>Cantar</Styled.ParticipantContent>
+          </Styled.ParticipantContentContainer>
+
+          <Input placeholder='informe a hora' />
+        </Styled.ContentGridContainer>
+      )
+    })
+  }
+
   return (
     <>
       <Header haveMenu={true} />
 
-      <h2>olá</h2>
+      <Styled.Container>
+        <ContainerPainel>
+          <ContainerHeader type='participants' />
+
+          <Styled.ContentContainer>
+            <Styled.ContentGridTitleContainer>
+              <Styled.GridTitle>Nome</Styled.GridTitle>
+              <Styled.GridTitle>Atividade</Styled.GridTitle>
+              <Styled.GridTitle>Hora</Styled.GridTitle>
+            </Styled.ContentGridTitleContainer>
+
+            {renderParticipants()}
+
+          </Styled.ContentContainer>
+
+          <Styled.PagesIconsContainer>
+            <Styled.GenericButton>
+              <Styled.ArrowLeftIcon />
+            </Styled.GenericButton>
+
+            <Styled.GenericButton>
+              <Styled.ArrowRightIcon />
+            </Styled.GenericButton>
+          </Styled.PagesIconsContainer>
+
+          <Styled.ButtonContainer>
+            <Styled.ConfirmButton>
+              <Styled.ArrowRightIconButton />
+            </Styled.ConfirmButton>
+          </Styled.ButtonContainer>
+        </ContainerPainel>
+      </Styled.Container>
     </>
   )
 }
---
+++ src/pages/CreateSchedule/styles.tsx
@@ -0,0 +1,159 @@
+import styled from 'styled-components'
+import {
+  FaArrowLeft,
+  FaArrowRight
+} from 'react-icons/fa'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 90vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-height: 600px) {
+    height: 88vh;
+  }
+
+  @media (max-height: 400px) {
+    height: 82vh;
+  }
+`
+
+export const ContentContainer = styled.div`
+  width: 80%;
+
+  margin-bottom: 10px;
+
+  background-color: transparent;
+`
+
+export const ContentGridTitleContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  margin-bottom: 15px;
+
+  align-content: center;
+`
+
+export const ContentGridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  margin-bottom: 2px;
+
+  align-content: center;
+`
+
+export const GridTitle = styled.h3`
+  font-size: 14px;
+  font-size: 0.875rem;
+  font-weight: bold;
+
+  justify-self: center;
+
+  color: ${props => props.theme.colors.containerFont};
+`
+
+export const ParticipantContentContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${props => props.theme.colors.tableContentBorder};
+`
+
+export const ParticipantContent = styled.p`
+  font-size: 14px;
+  font-size: 0.875rem;
+
+  color: ${props => props.theme.colors.containerFont};
+`
+
+// pagination
+export const GenericButton = styled.button`
+  border: 0 none;
+  background-color: transparent;
+
+  :focus {
+    box-shadow: 0 0 0 0;
+    outline: 0;
+  }
+
+  :active {
+    border-style: solid;
+    border-width: 1px;
+    border-color: #333;
+  }
+`
+
+export const PagesIconsContainer = styled.div`
+  height: 30px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const ArrowLeftIcon = styled(FaArrowLeft)`
+  margin-left: 15px;
+
+  font-size: ${props => props.theme.iconSize};
+
+  color: ${props => props.theme.colors.sideBarIcon};
+`
+
+export const ArrowRightIcon = styled(FaArrowRight)`
+  margin-right: 15px;
+
+  font-size: ${props => props.theme.iconSize};
+
+  color: ${props => props.theme.colors.sideBarIcon};
+`
+
+// confirm button
+export const ButtonContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`
+
+export const ConfirmButton = styled.button`
+  width: 100px;
+  height: 30px;
+
+  margin-right: 15px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 0 none;
+  border-radius: 10px;
+  background-color: ${props => props.theme.colors.buttonbackground};
+
+  :focus {
+    box-shadow: 0 0 0 0;
+    outline: 0;
+  }
+
+  :active {
+    border-style: solid;
+    border-width: 1px;
+    border-color: #333;
+  }
+`
+
+export const ArrowRightIconButton = styled(FaArrowRight)`
+  font-size: ${props => props.theme.iconSize};
+
+  color: ${props => props.theme.colors.buttonIcon};
+`
